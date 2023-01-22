9d1abbc7577b96ea9af4b86aa650ff3270238d28
Malin Dahlquist <m-dahlquist@hotmail.com>
2023-01-22 20:24:14 +0100

    add animation

---
+++ script.js
@@ -0,0 +1,16 @@
+gsap.to("#svg", {
+  scale: 0.5,
+  transformOrigin: "center center",
+  duration: 5,
+  repeat: -1,
+  yoyo: true,
+})
+gsap.to("#i", { opacity: 0, duration: 0.5, repeat: -1, yoyo: true })
+gsap.to("#l", { opacity: 0, duration: 0.6, repeat: -1, yoyo: true })
+gsap.to("#o", { opacity: 0, duration: 0.7, repeat: -1, yoyo: true })
+gsap.to("#v", { opacity: 0, duration: 0.8, repeat: -1, yoyo: true })
+gsap.to("#e", { opacity: 0, duration: 0.9, repeat: -1, yoyo: true })
+gsap.to("#d", { opacity: 0, duration: 1.0, repeat: -1, yoyo: true })
+gsap.to("#oo", { opacity: 0, duration: 1.1, repeat: -1, yoyo: true })
+gsap.to("#g", { opacity: 0, duration: 1.2, repeat: -1, yoyo: true })
+gsap.to("#s", { opacity: 0, duration: 1.3, repeat: -1, yoyo: true })
